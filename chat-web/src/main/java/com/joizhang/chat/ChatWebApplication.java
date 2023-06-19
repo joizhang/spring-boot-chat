160d318c0ddf419e55b334497402e5f9c2dc8d73
joizhang <zxlwhom@outlook.com>
2023-06-19 10:22:00 +0800

    feat: AutoConfiguration error

--- chat-web/src/main/java/com/joizhang/chat/ChatWebApplication.java
+++ chat-web/src/main/java/com/joizhang/chat/ChatWebApplication.java
@@ -1,17 +1,15 @@
 package com.joizhang.chat;
 
 import com.joizhang.chat.common.security.annotation.EnableResourceServer;
-import com.joizhang.chat.common.swagger.annotation.EnableSwaggerDoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableSwaggerDoc
 @EnableResourceServer
 @SpringBootApplication
-public class ChatApplication {
+public class ChatWebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChatApplication.class, args);
+        SpringApplication.run(ChatWebApplication.class, args);
     }
 
 }
